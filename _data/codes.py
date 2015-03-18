8c1938981121388691f456a13d7f84358fa2a08f
Daniel Wheeler <daniel.wheeler2@gmail.com>
2015-03-18 11:07:50 -0400

    Update codes.py to return code stats from Github

--- _data/codes.py
+++ _data/codes.py
@@ -8,17 +8,16 @@ class GitHubStats(object):
     def __init__(self, user, repo):
         github.Github
         self.base_url = 'https://api.github.com/repos/{user}/{repo}'.format(user=user, repo=repo)
-        self.issues = self.get_issues()
 
-    def get_issues(self):
-        issues = []        
-        url = self.base_url + '/issues'
+    def paginate(self, ext):
+        items = []        
+        url = self.base_url + ext
         while url:
             request = requests.get(url)
-            issues += request.json()
+            items += request.json()
             url = self.get_next_page(request)
-        return issues
-    
+        return items
+
     def get_next_page(self, request):
         link = request.headers['link'].split(',')[0]
         if 'rel="next"' in link:
@@ -31,7 +30,17 @@ class GitHubStats(object):
     def number_of_open_issues(self):
         print self.issues[0]
         return len([issue for issue in self.issues if issue['state'] == 'open'])
-            
+
+    def get_stats(self):
+        issues = self.paginate('/issue?state=open')
+        contributors = self.paginate('/stats/contributors')
+        commit_activity = self.paginate('/stats/commit_activity')
+        
+        return {
+            'open_issues' : len(issues),
+            'total_contributors' : len(contributors),
+            'yearly_commits' : commit_activity['total']
+            }
         
 def codes_yaml_to_json():
     data = yaml.load(open('_data/codes.yaml', 'r'))
@@ -41,7 +50,7 @@ def codes_yaml_to_json():
 if __name__ == '__main__':
     # codes_yaml_to_json()
     stats = GitHubStats('usnistgov', 'fipy')
-    print stats.number_of_open_issues
+    print get_stats
 
     
 
