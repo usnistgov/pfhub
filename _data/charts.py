91b96c2cd32e1e035e39836e49ab70c0c4e517d2
Daniel Wheeler <daniel.wheeler2@gmail.com>
2016-12-29 17:32:33 -0500

    Full implementation of chart data generation

--- _data/charts.py
+++ _data/charts.py
@@ -1,14 +1,61 @@
-from toolz.curried import map, pipe, compose, get, do, curry, count, pluck, juxt, flip, remove, filter, keyfilter, valfilter, valmap, itemmap
-import ruamel.yaml as yaml
+"""Data pipelines to parse benchmark YAML for charts.
+
+Extracts benchmark YAML and combines with template chart YAML for Vega
+charts. Run `python _data/charts.py` to build the charts.
+"""
+
 import glob
 import os
+import json
+
+from toolz.curried import map, pipe, get, curry, filter, valmap, itemmap, groupby # pylint: disable=redefined-builtin, no-name-in-module
+
+import ruamel.yaml as yaml
+
+
+def id_to_yaml():
+    """Maps benchmark id to the Vega YAML template.
+
+    Returns:
+      the mapping as a dict
+    """
+    return {'1a' : '1a_free_energy.yaml',
+            '1b' : '1b_free_energy.yaml'}
 
 def read_yaml(filepath):
+    """Read a YAML file
+
+    Args:
+      filepath: the path to the YAML file
+
+    Returns:
+      returns a dictionary
+    """
     with open(filepath) as stream:
         data = yaml.safe_load(stream)
-    return dict(name=os.path.split(os.path.split(filepath)[0])[1], data=data)
+    return data
+
+@curry
+def write_json(data, filepath):
+    """Write a JSON file
+
+    Args:
+      data: the dictionary to write
+      filepath: the path to the JSON file
+
+    Returns:
+      returns a tuple of (filepath, data)
+    """
+    with open(filepath, 'w') as stream:
+        json.dump(data, stream, sort_keys=True, indent=2)
+    return (filepath, data)
 
 def get_path():
+    """Return the local file path for this file.
+
+    Returns:
+      the filepath
+    """
     return pipe(
         __file__,
         os.path.realpath,
@@ -18,30 +65,95 @@ def get_path():
 
 @curry
 def update_dict(dict_, **kwargs):
+    """Add keys to a new dictionary.
+
+    Args:
+      dict_: the dictionary to add to
+      kwargs: the key, value pairs to add
+
+    Returns:
+      a new dictionary
+    """
     return dict(list(dict_.items()) + list(kwargs.items()))
 
+def filter_data(yaml_data):
+    """Extract the free_energy data from the YAML files.
 
-data = pipe(
-    os.path.join(get_path(), 'benchmarks/*/meta.yaml'),
-    glob.glob,
-    map(read_yaml),
-    dict,
-    keyfilter(lambda key: key != 'example'),
-    groupby()
-    valfilter(lambda val: val['benchmark']['id'] == '1a'),
-    valmap(lambda val: val['data']),
-    valmap(filter(lambda item: item['name'].lower() == 'free_energy')),
-    valmap(list),
-    valmap(get(0)),
-    itemmap(lambda item: (item[0], update_dict(item[1], name=item[0]))),
-    lambda dict_: list(dict_.values())
-    # itemmap(lambda key, val:, (key, list(dict(**vals).items()) +
-    # filter(lambda data: data['data']['benchmark']['id'].lower() == '1a'),
-    # map(lambda data: dict(name=data['name'], data=data['data']['data'])),
-    # # map(filter(lambda data: data['data']['name'].lower() == 'free_energy')),
-    # # map(list),
-)
+    Args:
+      yaml_data: the benchmark YAML data
 
-print(data)
+    Returns:
+      the free_energy data from the YAML data
+    """
+    return pipe(
+        yaml_data,
+        dict,
+        valmap(lambda val: val['data']),
+        valmap(filter(lambda item: item['name'].lower() == 'free_energy')),
+        valmap(list),
+        valmap(get(0)),
+        itemmap(lambda item: (item[0], update_dict(item[1], name=item[0]))),
+        lambda dict_: list(dict_.values())
+    )
 
-print(update_dict(dict(a=1, b=3), a=2, b=4))
+def get_data():
+    """Read in the YAML data and group by benchmark id
+
+    Returns:
+      a dictionary with benchmark ids as keys and lists of appropriate
+      data for values
+    """
+    return pipe(
+        os.path.join(get_path(), 'benchmarks/*/meta.yaml'),
+        glob.glob,
+        map(lambda path_: (os.path.split(os.path.split(path_)[0])[1], read_yaml(path_))),
+        filter(lambda item: item[0] != 'example'),
+        groupby(lambda item: item[1]['benchmark']['id']),
+        valmap(filter_data),
+    )
+
+def get_chart(key):
+    """Read the chart YAML
+
+    Args:
+      key: the benchmark id
+
+    Returns:
+      the chart template
+
+    """
+    return read_yaml(os.path.join(get_path(), 'charts', id_to_yaml()[key]))
+
+def write_chart_json(item):
+    """Write a chart JSON file.
+
+    Args:
+      item: a (benchmark_id, chart_dict) pair
+
+    Returns:
+      returns the (filepath, json_data) pair
+    """
+    return pipe(
+        item[0],
+        lambda id_: id_to_yaml()[id_].replace('.yaml', '.json'),
+        lambda file_: os.path.join(get_path(), '../data/charts', file_),
+        write_json(item[1]) # pylint: disable=no-value-for-parameter
+    )
+
+def main():
+    """Generate the chart JSON files
+
+    Returns:
+      list of (filepath, chart_json) pairs
+    """
+    return pipe(
+        get_data(),
+        itemmap(
+            lambda item: (item[0],
+                          update_dict(get_chart(item[0]), data=item[1]))
+        ),
+        itemmap(write_chart_json)
+    )
+
+if __name__ == "__main__":
+    main()
