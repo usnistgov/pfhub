90bb45d35a9b8edc84cac82f95c71132880c10b5
Daniel Wheeler <daniel.wheeler2@gmail.com>
2018-08-27 15:36:20 -0400

    testing

--- comment.sh
+++ comment.sh
@@ -1,9 +1,12 @@
-#! /bin/sh
+#!/bin/bash
 
 COMMENT="Testing commenting from Travis CI"
 
-echo "got here"
-if [ "$TRAVIS_PULL_REQUEST" = "true" ]; then
+echo "got here 0"
+echo "$TRAVIS_PULL_REQUEST"
+if [ "$TRAVIS_PULL_REQUEST" = "true" ]
+then
+  echo "git here 1"
   echo "${TRAVIS_REPO_SLUG}"
   curl -H "Authorization: token ${GITHUB_TOKEN}" -X POST -d "{\"body\": \"$COMMENT\"}" "https://api.github.com/repos/${TRAVIS_REPO_SLUG}/issues/${TRAVIS_PULL_REQUEST}/comments";
 fi
