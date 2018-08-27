2655ba6489539fa1dd46c9b04aed02d989f0128c
Daniel Wheeler <daniel.wheeler2@gmail.com>
2018-08-27 15:54:19 -0400

    try different github token

--- comment.sh
+++ comment.sh
@@ -8,5 +8,6 @@ if [ "$TRAVIS_PULL_REQUEST" != "false" ]
 then
   echo "git here 1"
   echo "${TRAVIS_REPO_SLUG}"
-  curl -H "Authorization: token ${GITHUB_TOKEN}" -X POST -d "{\"body\": \"$COMMENT\"}" "https://api.github.com/repos/${TRAVIS_REPO_SLUG}/issues/${TRAVIS_PULL_REQUEST}/comments";
+  echo "${GITHUB_TOKEN_1}"
+  curl -H "Authorization: token ${GITHUB_TOKEN_1}" -X POST -d "{\"body\": \"$COMMENT\"}" "https://api.github.com/repos/${TRAVIS_REPO_SLUG}/issues/${TRAVIS_PULL_REQUEST}/comments";
 fi
