22291d95d5996f568bec9f1c23f547e1a8d466b6
Daniel Wheeler <daniel.wheeler2@gmail.com>
2018-08-28 10:12:40 -0400

    Clean up comments

--- comment.sh
+++ comment.sh
@@ -2,12 +2,7 @@
 
 COMMENT="Testing commenting from Travis CI"
 
-echo "got here 0"
-echo "$TRAVIS_PULL_REQUEST"
 if [ "$TRAVIS_PULL_REQUEST" != "false" ]
 then
-  echo "git here 1"
-  echo "${TRAVIS_REPO_SLUG}"
-  echo "${GITHUB_TOKEN_1}"
-  curl -H "Authorization: token ${GITHUB_TOKEN_1}" -X POST -d "{\"body\": \"$COMMENT\"}" "https://api.github.com/repos/${TRAVIS_REPO_SLUG}/issues/${TRAVIS_PULL_REQUEST}/comments";
+  curl -H "Authorization: token ${GITHUB_TOKEN}" -X POST -d "{\"body\": \"$COMMENT\"}" "https://api.github.com/repos/${TRAVIS_REPO_SLUG}/issues/${TRAVIS_PULL_REQUEST}/comments";
 fi
