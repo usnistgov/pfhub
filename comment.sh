5813603428fc31d1b29c7573c1bf848e175fd302
Daniel Wheeler <daniel.wheeler2@gmail.com>
2018-08-28 12:50:40 -0400

    Testing getting the github_id from meta.yaml

--- comment.sh
+++ comment.sh
@@ -2,15 +2,17 @@
 
 COMMENT="Testing commenting from Travis CI"
 
-echo ""
-echo "check stuff"
+PULL_REQUST_URL="https://api.github.com/repos/usnistgov/pfhub/issues/${TRAVIS_PULL_REQUST}"
 
-ls $TRAVIS_BUILD_DIR/_data/simulations
-
-echo "uncheck stuff"
-echo ""
-
+SIM_NAME=`curl -X GET "${PULL_REQUEST_URL}" | jq -r '.title' | sed -e 's/PFHub Upload: //'`
+echo $SIM_NAME
+META_YAML="${TRAVIS_BUILD_DIR}/_data/simulations/${SIM_NAME}/meta.yaml"
+echo $META_YAML
+GITHUB_ID=`cat ${META_YAML} | yq .metadata.author.github_id`
+echo $GITHUB_ID
+COMMENT="@${GITHUB_ID} testing comments..."
+echo $COMMENT
 if [ "$TRAVIS_PULL_REQUEST" != "false" ]
 then
-  curl -H "Authorization: token ${GITHUB_TOKEN}" -X POST -d "{\"body\": \"$COMMENT\"}" "https://api.github.com/repos/${TRAVIS_REPO_SLUG}/issues/${TRAVIS_PULL_REQUEST}/comments";
+  curl -H "Authorization: token ${GITHUB_TOKEN}" -X POST -d "{\"body\": \"$COMMENT\"}" "${PULL_REQUEST_URL}/comments";
 fi
