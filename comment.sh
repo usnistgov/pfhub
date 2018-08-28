a73c5f610070711df1d1d9ac2af32d9637b4a907
Daniel Wheeler <daniel.wheeler2@gmail.com>
2018-08-28 15:46:28 -0400

    more testing

--- comment.sh
+++ comment.sh
@@ -1,20 +1,17 @@
 #!/bin/bash
 
-COMMENT="Testing commenting from Travis CI"
-
-echo "$TRAVIS_PULL_REQUEST"
-PULL_REQUEST_URL="https://api.github.com/repos/${TRAVIS_REPO_SLUG}/issues/${TRAVIS_PULL_REQUEST}"
-echo "$PULL_REQUEST_URL"
-SIM_NAME=`curl -X GET "${PULL_REQUEST_URL}" | jq -r '.title' | sed -e 's/PFHub Upload: //'`
-echo "$SIM_NAME"
-META_YAML="${TRAVIS_BUILD_DIR}/_data/simulations/${SIM_NAME}/meta.yaml"
-echo "$META_YAML"
-TEMP=`cat ${META_YAML} | yq .metadata.author.github_id`
-GITHUB_ID=`sed -e 's/\"//g' <<< ${TEMP}`
-echo $GITHUB_ID
-COMMENT="@${GITHUB_ID} testing comments..."
-echo $COMMENT
 if [ "$TRAVIS_PULL_REQUEST" != "false" ]
 then
-  curl -H "Authorization: token ${GITHUB_TOKEN}" -X POST -d "{\"body\": \"$COMMENT\"}" "${PULL_REQUEST_URL}/comments";
+  STRING=`echo "${TRAVIS_PULL_REQUEST_BRANCH}" | cut -c1-9`
+  echo $STRING
+  if [ ${STRING} = "staticman" ]
+  then
+    PULL_REQUEST_URL="https://api.github.com/repos/${TRAVIS_PULL_REQUEST_SLUG}/issues/${TRAVIS_PULL_REQUEST}";
+    SIM_NAME=`curl -X GET "${PULL_REQUEST_URL}" | jq -r '.title' | sed -e 's/PFHub Upload: //'`;
+    META_YAML="${TRAVIS_BUILD_DIR}/_data/simulations/${SIM_NAME}/meta.yaml";
+    TEMP=`cat ${META_YAML} | yq .metadata.author.github_id`;
+    GITHUB_ID=`sed -e 's/\"//g' <<< ${TEMP}`;
+    COMMENT="@${GITHUB_ID} testing comments...";
+    curl -H "Authorization: token ${GITHUB_TOKEN}" -X POST -d "{\"body\": \"$COMMENT\"}" "${PULL_REQUEST_URL}/comments";
+  fi
 fi
