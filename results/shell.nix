fe110da89d4330efa076637500feb39614ea2714
Daniel Wheeler <daniel.wheeler2@gmail.com>
2021-10-15 17:34:02 -0400

    build(nix):
    Make nbextension addons for Jupyter install correctly.

--- results/shell.nix
+++ results/shell.nix
@@ -24,6 +24,15 @@ in
     ];
 
     shellHook = ''
+      SOURCE_DATE_EPOCH=$(date +%s)
+      export PYTHONUSERBASE=$PWD/.local
+      export USER_SITE=`python -c "import site; print(site.USER_SITE)"`
+      export PYTHONPATH=$PYTHONPATH:$USER_SITE
+      export PATH=$PATH:$PYTHONUSERBASE/bin
+
+      export OMPI_MCA_plm_rsh_agent="${pkgs.openssh.out}"/bin/ssh
+      export PETSC_DIR="${pkgs.petsc.out}"
+      export NIX_SSL_CERT_FILE=/etc/ssl/certs/ca-certificates.crt
 
       jupyter nbextension install --py widgetsnbextension --user
       jupyter nbextension enable widgetsnbextension --user --py
@@ -32,14 +41,5 @@ in
       jupyter contrib nbextension install --user > /dev/null 2>&1
       jupyter nbextension enable spellchecker/main > /dev/null 2>&1
 
-      SOURCE_DATE_EPOCH=$(date +%s)
-      export PYTHONUSERBASE=$PWD/.local
-      export USER_SITE=`python -c "import site; print(site.USER_SITE)"`
-      export PYTHONPATH=$PYTHONPATH:$USER_SITE
-      export PATH=$PATH:$PYTHONUSERBASE/bin
-      export OMPI_MCA_plm_rsh_agent="${pkgs.openssh.out}"/bin/ssh
-      export PETSC_DIR="${pkgs.petsc.out}"
-
-      export NIX_SSL_CERT_FILE=/etc/ssl/certs/ca-certificates.crt
     '';
   }
