73104de966cd0a7d3a65321b79b33da93e6d2d1b
Daniel Wheeler <daniel.wheeler2@gmail.com>
2022-03-29 18:06:37 -0400

    fix: remove impure jupyter installs
    Use a dedicated Jupyter environment from a stable location

--- _data/python-pfhub/shell.nix
+++ _data/python-pfhub/shell.nix
@@ -9,13 +9,15 @@ let
   pkgs = import (builtins.fetchTarball "https://github.com/NixOS/nixpkgs/archive/${tag}.tar.gz") {};
   pypkgs = pkgs.python3Packages;
   pfhub = pypkgs.callPackage ./default.nix {};
-  extra = with pypkgs; [ ipywidgets black pylint flake8 ];
+  extra = with pypkgs; [ black pylint flake8 ];
+  jupyter_src = builtins.fetchTarball "https://github.com/wd15/nixes/archive/refs/heads/master.zip";
+  jupyter_extra = pypkgs.callPackage "${jupyter_src}/jupyter/default.nix" { };
 in
   (pfhub.overridePythonAttrs (old: rec {
 
     propagatedBuildInputs = old.propagatedBuildInputs;
 
-    nativeBuildInputs = propagatedBuildInputs ++ extra;
+    nativeBuildInputs = propagatedBuildInputs ++ extra ++ [ jupyter_extra ];
 
     postShellHook = ''
       SOURCE_DATE_EPOCH=$(date +%s)
@@ -23,18 +25,6 @@ in
       export USER_SITE=`python -c "import site; print(site.USER_SITE)"`
       export PYTHONPATH=$PYTHONPATH:$USER_SITE:$(pwd)
       export PATH=$PATH:$PYTHONUSERBASE/bin
-
-      # export OMPI_MCA_plm_rsh_agent="${pkgs.openssh.out}"/bin/ssh
-      # export PETSC_DIR="${pkgs.petsc.out}"
       export NIX_SSL_CERT_FILE=/etc/ssl/certs/ca-certificates.crt
-
-      jupyter nbextension install --py widgetsnbextension --user
-      jupyter nbextension enable widgetsnbextension --user --py
-      pip install jupyter_contrib_nbextensions --user > /dev/null 2>&1
-      pip install jupyter_dash --user > /dev/null 2>&1
-      jupyter contrib nbextension install --user > /dev/null 2>&1
-      jupyter nbextension enable spellchecker/main > /dev/null 2>&1
-
-      pip install itables --user > /dev/null 2>&1
     '';
   }))
