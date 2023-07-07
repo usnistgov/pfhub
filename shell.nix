58233d13826ceb2c5d10659e5a0125750c1f1cfb
Daniel Wheeler <daniel.wheeler2@gmail.com>
2023-07-07 13:47:22 -0400

    build(nix): remove all old nix builds

--- shell.nix
+++
@@ -1,8 +0,0 @@
-let
-  nixpkgs = import ./_nix/nixpkgs_version.nix;
-  extra = [
-    nixpkgs.python36Packages.jupyter
-    nixpkgs.python36Packages.bokeh
-  ];
-in
-  import ./_nix/env.nix { inherit nixpkgs; inherit extra; }
